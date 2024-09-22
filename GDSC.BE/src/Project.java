5cf8dbc5f261482271192a67dd430e208762363d
김규남 <150914407+gyu-nam@users.noreply.github.com>
2024-09-22 20:39:17 +0900

    다시 main으로 머지해보겠습니다.

---
+++ GDSC.BE/src/Project.java
@@ -0,0 +1,12 @@
+public class Project extends Activity {
+    private String deadline;
+
+    public Project(String activity_name, String explanation, String deadline) {
+        super(activity_name, explanation);
+        this.deadline = deadline;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+}
---
+++ GDSC.BE/src/Study.java
@@ -0,0 +1,5 @@
+public class Study extends Activity {
+    public Study(String activity_name, String explanation) {
+        super(activity_name, explanation);
+    }
+}
