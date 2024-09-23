e64eee522a361fee9abb45758b76775239b34c8d
조원준 <157904784+jwjdd@users.noreply.github.com>
2024-09-23 17:26:48 +0900

    Update PersonalInformation.java

--- GDSC.BE/src/PersonalInformation.java
+++ GDSC.BE/src/PersonalInformation.java
@@ -3,22 +3,18 @@ public class PersonalInformation{
     public final String name;
     public String department;
     public String major;
-    public String field;
     public String gitId;
     public String interestedField;
     public String introduction;
-    public String skill;
 
     // 생성자
-    public PersonalInformation(String name, String department, String major, String field, String gitId, String interestedField, String introduction, String skill) {
+    public PersonalInformation(String name, String department, String major, String gitId, String interestedField, String introduction) {
         this.name = name;
         this.department = department;
         this.major = major;
-        this.field = field;
         this.gitId = gitId;
         this.interestedField = interestedField;
         this.introduction = introduction;
-        this.skill = skill;
     }
 
     // 인적 사항 출력
@@ -27,12 +23,9 @@ public class PersonalInformation{
         System.out.println("Name: " + name);
         System.out.println("Department: " + department);
         System.out.println("Major: " + major);
-        System.out.println("Field: " + field);
         System.out.println("Git ID: " + gitId);
         System.out.println("Interested Field: " + interestedField);
         System.out.println("Introduction: " + introduction);
-        System.out.println("Skill: " + skill);
-        System.out.println("Extra Information: " + extraInfo); // 제네릭 정보를 출력
     }
 
 }
