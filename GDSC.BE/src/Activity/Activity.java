ff8d1a0498c4f9719818fc70a60e13b2aa36eb20
김규남\ggn11 <ggn1113@naver.com>
2024-09-23 17:04:01 +0900

    update activity class

--- GDSC.BE/src/Activity/Activity.java
+++ GDSC.BE/src/Activity/Activity.java
@@ -1,6 +1,9 @@
 package Activity;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import Exception.*;
 
 public abstract class Activity {
     enum Status {
@@ -9,49 +12,55 @@ public abstract class Activity {
         Completed
     }
     protected Status status;
-    private String activity_name;
+    private String activityName;
     private String explanation;
     private List<String> members;
 
-    public Activity(String activity_name, String explanation) {
-        this.activity_name = activity_name;
+    public Activity(String activityName, String explanation) {
+        this.activityName = activityName;
         this.explanation = explanation;
         this.members = new ArrayList<String>();
     }
 
-    public String getActivity_name() {
-        return activity_name;
+    public String getActivityName() {
+        return activityName;
     }
 
-    public void addMember(String member){
-        members.add(member);
-        System.out.println(member + " has been added to the list.");
+    public void addMember(String member) throws MemberAlreadyExistsException {
+       if(members.contains(member)){
+           throw new MemberAlreadyExistsException(member + "is already existed");
+        } else{
+           members.add(member);
+           System.out.println(member + " has been added to the list.");
+       }
+
     }
 
-    public void removeMember(String member){
+    public void removeMember(String member) throws MemberNotExistException {
         if(members.contains(member)){
             members.remove(member);
             System.out.println(member + " has been removed from the list.");
         } else{
-            System.out.println(member + " has not existed");
+            throw new MemberNotExistException(member + "is not existed");
 
         }
     }
 
-    public void showMembers(){
+    public Optional<List<String>> showMembers(){
         if(members.isEmpty()){
-            System.out.println(activity_name + "is empty");
-        }
-        else{
+            System.out.println(activityName + "is empty");
+            return Optional.empty();
+        } else{
             for(String member : members){
                 System.out.println(member + ",");
             }
+            return Optional.of(members);
         }
     }
 
     public void setStatus(Status status) {
         this.status = status;
-        System.out.println(getActivity_name() + "의 상태가 " + status + "로 설정되었습니다.");
+        System.out.println(getActivityName() + "의 상태가 " + status + "로 설정되었습니다.");
     }
 
     public Status getStatus() {
