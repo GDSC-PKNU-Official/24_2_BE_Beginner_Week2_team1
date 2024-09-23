ec28ab56a6cf3a1937b2084e593ef0deeb47621d
sierra <sierra3144@naver.com>
2024-09-23 19:41:21 +0900

    안내 문구 상수로 분리

--- GDSC.BE/src/Main.java
+++ GDSC.BE/src/Main.java
@@ -3,14 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         ProgramManager pm = new ProgramManager();
-        System.out.println("Welcome to GDSC PKNU!");
-        System.out.println("\n\nChoose an option:");
-        System.out.println("member: Show Member List");
-        System.out.println("study: Show Study List");
-        System.out.println("project: Show Project List");
-        System.out.println("info: Show Community Information");
-        System.out.println("event: Show Event List");
-        System.out.println("exit: Exit Program\n\n");
+        System.out.println(InitialInformation.GREETING);
 
         while(true){
             Scanner sc = new Scanner(System.in);
