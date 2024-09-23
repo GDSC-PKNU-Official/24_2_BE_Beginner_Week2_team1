2f0914c464ab27d7553e8bd2318355017a84f577
hatta <zaangkr1@gmail.com>
2024-09-23 19:48:39 +0900

    personalinfo에 genelic을 통해 사용자 입력 받을 수 있도록...

--- GDSC.BE/src/PersonalInformation.java
+++ GDSC.BE/src/PersonalInformation.java
@@ -1,21 +1,31 @@
-public class PersonalInformation{
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class PersonalInformation<K, V>{
     // 필드 (속성)
     public final String name;
     public String department;
     public String major;
-    public String gitId;
-    public String interestedField;
-    public String introduction;
+    public Map<K, V> additionalInfo;
+
+
 
     // 생성자
-    public PersonalInformation(String name, String department, String major, String gitId, String interestedField, String introduction) {
+    public PersonalInformation(String name, String department, String major) {
         this.name = name;
         this.department = department;
         this.major = major;
-        this.gitId = gitId;
-        this.interestedField = interestedField;
-        this.introduction = introduction;
+        this.additionalInfo = new HashMap<>();
+
     }
+
+
+    // 추가적인 정보를 추가할 수 있는 메서드
+    public void addAdditionalInfo(K key, V value) {
+        additionalInfo.put(key, value);
+    }
+
 
     // 인적 사항 출력
     public void printInfo() {
@@ -23,9 +33,15 @@ public class PersonalInformation{
         System.out.println("Name: " + name);
         System.out.println("Department: " + department);
         System.out.println("Major: " + major);
-        System.out.println("Git ID: " + gitId);
-        System.out.println("Interested Field: " + interestedField);
-        System.out.println("Introduction: " + introduction);
+
+        // 추가적인 정보 출력
+        if (!additionalInfo.isEmpty()) {
+            System.out.println("Additional Information:");
+            for (Map.Entry<K, V> entry : additionalInfo.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+        }
     }
 
-}
+    }
+
