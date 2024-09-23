bf71f8fbc641dd24bc5a5cf67c99fda43261547c
sierra <sierra3144@naver.com>
2024-09-23 22:02:30 +0900

    exceptional 추가

--- GDSC.BE/src/CommandHelper.java
+++ GDSC.BE/src/CommandHelper.java
@@ -24,30 +24,30 @@ public class CommandHelper {
     }
 
     private void addEvents() {
-        Events e1=new Events(new Date(2024,9,12,18,0), "깃세션", "공간숲");
+        Events e1 = new Events(new Date(2024, 9, 12, 18, 0), "깃세션", "공간숲");
         events.add(e1);
-        Events e2=new Events(new Date(2024,10,28,20,30), "정기모임", "장보고관");
+        Events e2 = new Events(new Date(2024, 10, 28, 20, 30), "정기모임", "장보고관");
         events.add(e2);
     }
 
     private void addStudies() {
-        Study s1=new Study("백엔드 스터디", "스프링을 공부합니다.");
+        Study s1 = new Study("백엔드 스터디", "스프링을 공부합니다.");
         studies.add(s1);
-        Study s2=new Study("프론트엔드 스터디", "리액트를 공부합니다.");
+        Study s2 = new Study("프론트엔드 스터디", "리액트를 공부합니다.");
         studies.add(s2);
     }
 
     private void addProjects() {
-        Project p1=new Project("백엔드 프로젝트", "백엔드 프로젝트 설명", "2024-12-19");
+        Project p1 = new Project("백엔드 프로젝트", "백엔드 프로젝트 설명", "2024-12-19");
         projects.add(p1);
-        Project p2=new Project("프론트엔드 프로젝트", "프론트엔드 프로젝트 설명", "2024-11-24");
+        Project p2 = new Project("프론트엔드 프로젝트", "프론트엔드 프로젝트 설명", "2024-11-24");
         projects.add(p2);
     }
 
     private void addPeople() {
         Person p1 = new Person(new PersonalInformation("김서영", "컴퓨터인공지능공학부", "컴퓨터공학전공"), new Coremember("임베디드", "커뮤니티 매니징"));
         people.add(p1);
-        Person p2 = new Person(new PersonalInformation("조원준","전자정보통신공학부"), new Beginner("백엔드","백엔드"));
+        Person p2 = new Person(new PersonalInformation("조원준", "전자정보통신공학부"), new Beginner("백엔드", "백엔드"));
         people.add(p2);
         Person p3 = new Person(new PersonalInformation("김규남", "컴퓨터인공지능공학부", "컴퓨터공학전공"), new Member("백엔드"));
         people.add(p3);
